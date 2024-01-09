26fdfb5faaf8dc93b01bf8c44eaac0326e430125
Brandon Arp <brandonarp@gmail.com>
2024-01-08 16:07:47 -0800

    remove dist directory

--- dist/beans/aggregators/parameters/sampling_aggregator_parameter.d.ts
+++
@@ -1,7 +0,0 @@
-import { AggregatorParameterType } from "./aggregator_parameter";
-import { AnyAggregatorParameter } from "./any_aggregator_parameter";
-export declare class SamplingAggregatorParameter extends AnyAggregatorParameter {
-    static TYPE: AggregatorParameterType;
-    static fromObject(object: any): SamplingAggregatorParameter;
-    constructor(text?: string, value?: any);
-}
