2e03ced2087524494582444113d9ec1e04a5ec9f
Brandon Arp <brandonarp@gmail.com>
2024-01-12 02:00:20 -0800

    some more test fixes

--- src/core/request/query_builder.ts
+++ src/core/request/query_builder.ts
@@ -1,4 +1,4 @@
-import dateMath from "@grafana/data";
+import {dateMath} from "@grafana/data";
 import _ from "lodash";
 import {Moment} from "moment";
 import {Aggregator} from "../../beans/aggregators/aggregator";
